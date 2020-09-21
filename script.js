b7becdb5d87a74bad0e709d843093a097b147d78
Veronika <oncotorax19@gmail.com>
2020-09-21 17:10:26 +0300

    dghdgfd

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-alert('Hello');
+// step 1
+// Earn massage from server
+function getMassageFromServer (){
+
+}
+// Create massage code
+// add letter to massage-wrapper
+// earn text fron massage-author
+// earn text from massage
+// when clicking on button send, send massage-author:massage and make var 1
+//
